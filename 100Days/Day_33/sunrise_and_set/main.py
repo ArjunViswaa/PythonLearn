b025c70f1a15dc00e166d2306f1c597cb609eb08
Arjun Viswaa <arjunviswaa@gmail.com>
2025-05-06 16:04:52 +0530

    Python Learn Day 33

---
+++ 100Days/Day_33/sunrise_and_set/main.py
@@ -0,0 +1,20 @@
+import requests
+from datetime import datetime
+
+MY_LAT = 11.0149
+MY_LONG = 76.9554
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now()
